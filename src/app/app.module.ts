c1a750091c10029c220ef6c70dc8b69eeaf6bfcb
Judy Lipinski <judy@karmoxie.com>
2019-05-03 13:32:16 -0400

    added use of injector at component level

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { SpecialPeopleComponent } from './special-people/special-people.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SpecialPeopleComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
