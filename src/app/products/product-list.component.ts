35f999396271472f2711b2ae990719bb6995451e
Judy Lipinski <judy@karmoxie.com>
2019-05-04 16:17:04 -0400

    added different ids to nto be incremental

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -10,12 +10,12 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
     this.products = [
-      { id: 1, description: 'cups', quantity: 30 },
-      { id: 2, description: 'spoons', quantity: 10000 },
-      { id: 3, description: 'knives', quantity: 0 },
-      { id: 4, description: 'notebooks', quantity: 15 },
-      { id: 5, description: 'bottles', quantity: 3 },
-      { id: 6, description: 'crystals', quantity: 1 }
+      { id: 1423, description: 'cups', quantity: 30 },
+      { id: 1232, description: 'spoons', quantity: 10000 },
+      { id: 353, description: 'knives', quantity: 0 },
+      { id: 324, description: 'notebooks', quantity: 15 },
+      { id: 1435, description: 'bottles', quantity: 3 },
+      { id: 126, description: 'crystals', quantity: 1 }
     ];
   }
 
