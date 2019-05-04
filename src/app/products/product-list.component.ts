611cc536eb26a6403107b14c4817050ad84e0428
Judy Lipinski <judy@karmoxie.com>
2019-05-04 17:06:35 -0400

    added interval to change products rm trackby

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from './product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html'
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
+  intervalId: any;
+  differentQty = 10;
 
   ngOnInit() {
     this.products = [
@@ -17,9 +19,31 @@ export class ProductListComponent implements OnInit {
       { id: 1435, description: 'bottles', quantity: 3 },
       { id: 126, description: 'crystals', quantity: 1 }
     ];
+
+    this.intervalId = setInterval(() => {
+      this.products = [
+        { id: 1423, description: 'cups', quantity: this.differentQty++ },
+        { id: 1232, description: 'spoons', quantity: 10000 },
+        { id: 353, description: 'knives', quantity: 0 },
+        { id: 324, description: 'notebooks', quantity: this.differentQty++ },
+        { id: 1435, description: 'bottles', quantity: 3 },
+        { id: 126, description: 'crystals', quantity: 1 }
+      ];
+
+
+    }, 1000);
+
+    setTimeout(() => { clearInterval(this.intervalId); }, 20000);
+
   }
 
   trackByFunction(index: number, product: Product): number {
     return product.id;
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    clearInterval(this.intervalId);
   }
 }
