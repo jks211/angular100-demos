1b4bc9567215904f8e3190b77f8c753136d1a9f5
Judy Lipinski <judy@karmoxie.com>
2019-05-10 12:50:27 -0400

    updated simple observabel turned off line breaks

--- src/app/observables/simple-observable.component.ts
+++ src/app/observables/simple-observable.component.ts
@@ -3,18 +3,18 @@ import { from, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-from-operator',
-  template: `<button (click)="getData()">Start Subscribe</button>
-            {{numbers}}
-            `
+  template: `<button (click)="getData()">Start Subscribe</button>{{numbers}}`
 })
 export class SimpleObservableComponent {
   numbers = [];
-  // emit number values when subscribed to
+  // emit number values, in turn, when subscribed to
   arraySource: Observable<number> = from([1, 2, 3, 4]);
 
   getData() {
     // when clicked, this function subscribes, values added to array
-    this.arraySource.subscribe(values => (this.numbers = [...this.numbers, values]));
+    this.arraySource.subscribe(emittedValue => { console.log(`values`, emittedValue);
+              (this.numbers = [...this.numbers, emittedValue]);
+          });
   }
 }
 
