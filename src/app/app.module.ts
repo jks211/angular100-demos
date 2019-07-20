de28094dd584063c9d7c4554c3910c5a05bd5d20
Judy Lipinski <judy@karmoxie.com>
2019-07-20 17:50:47 -0400

    rm unnecessary compoentns from module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserListComponent } from './users/user-list.component';
-import { UserCardComponent } from './users/user-card.component';
-import { UserTableComponent } from './users/user-table.component';
-import { UserRowComponent } from './users/user-row.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NotfoundComponent } from './notfound.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UserListComponent, UserCardComponent, UserTableComponent, UserRowComponent,  NavbarComponent, NotfoundComponent, ContactComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, NotfoundComponent, ContactComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
