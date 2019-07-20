7b31ebdb32e1093f14542f7ab34e6821d55695bf
Judy Lipinski <judy@karmoxie.com>
2019-07-20 15:58:07 -0400

    shortened example to givefocus on less components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductListComponent }
-  from './products/product-list.component';
 import { UserListComponent } from './users/user-list.component';
-import { ObservablesExampleComponent }
-  from './observables/observables-example.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "users", component: UserListComponent },
-  { path: "products", component: ProductListComponent },
-  { path: "observables", component: ObservablesExampleComponent },
   { path: "**", redirectTo: "/users" }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserCardComponent } from './users/user-card.component';
-import { ProductRowComponent } from './products/product-row.component';
 import { UserTableComponent } from './users/user-table.component';
 import { UserRowComponent } from './users/user-row.component';
-import { NgCodeExamplesComponent } from './ng-code-examples/ng-code-examples.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { ObservablesExampleComponent } from './observables/observables-example.component';
-import { EvensObservableComponent } from './observables/evens-observable.component';
-import { OddsObservableComponent } from './observables/odds-observable.component';
-import { SimpleObservableComponent } from './observables/simple-observable.component';
-import { OperatorsObservableComponent } from './observables/operators-observable.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProductListComponent, UserListComponent, UserCardComponent, ProductRowComponent, ObservablesExampleComponent, UserTableComponent, UserRowComponent, NgCodeExamplesComponent, NavbarComponent, EvensObservableComponent, SimpleObservableComponent, OddsObservableComponent, OperatorsObservableComponent],
+  declarations: [AppComponent, HomeComponent, UserListComponent, UserCardComponent, UserTableComponent, UserRowComponent,  NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
