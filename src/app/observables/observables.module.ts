b7c4d5f63effd1db4fd645d74f557d31f00c309c
Judy Lipinski <judy@karmoxie.com>
2019-07-21 10:25:48 -0400

    updated

--- src/app/observables/observables.module.ts
+++ src/app/observables/observables.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObservablesExampleComponent } from './observables-example.component';
-import { EvensObservableComponent } from './evens/evens-observable.component';
+import { EvensObservableComponent } from './evens-observable.component';
 import { OperatorsObservableComponent } from './operators-observable.component';
 import { SimpleObservableComponent } from './simple-observable.component';
 import { OddsObservableComponent } from './odds-observable.component';
@@ -9,6 +9,7 @@ import { OddsObservableComponent } from './odds-observable.component';
 
 
 @NgModule({
+  // tslint:disable-next-line: max-line-length
   declarations: [ObservablesExampleComponent,EvensObservableComponent, SimpleObservableComponent, OddsObservableComponent, OperatorsObservableComponent],
   imports: [
     CommonModule
