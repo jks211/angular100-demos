aa5593bd244d5929afd2fe49c8cae06211179618
Judy Lipinki <judy@karmoxie.com>
2019-07-20 23:47:13 -0400

    updated to 8.1.2

--- src/app/shared/people.service.ts
+++ src/app/shared/people.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+
 import { Observable } from "rxjs";
+import { Person } from "./models/person.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class PeopleService {
-  url = "http://localhost:3000/people";
+  url = "http://localhost:5566/people";
 
   constructor(private http: HttpClient) {}
 
   getPeople(): Observable<Person[]> {
-    return this.http.get(this.url);
+    return this.http.get<Person[]>(this.url);
   }
 }
